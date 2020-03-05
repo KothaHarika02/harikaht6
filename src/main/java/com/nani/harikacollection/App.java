d8ce2bf8abe3dc95f77d80dee7f4fca54d9b920b
KothaHarika02 <kothaharika2000@gmail.com>
2020-03-04 22:35:39 -0800

    adding project file

---
+++ src/main/java/com/nani/harikacollection/App.java
@@ -0,0 +1,13 @@
+package com.nani.harikacollection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
